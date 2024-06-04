d254631f6939c642689283bf89507cd01df0252f
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2024-06-03 23:52:27 -0400

    agregado ejercicio no completado ejemplo entrevista

---
+++ ejercicios entrevista.py
@@ -0,0 +1,10 @@
+#definir una funcion max() que tome como argumento 2 numeros
+#y devuelva el valor mayor de ellos.
+#(python ya tiene una funcion max incorporada, esto es practica)
+
+def max(num1, num2):
+    if(!num1==int(num1) || !num2==int(num2))
+    if (num1 > num2):
+        return num1
+    else:
+        num2
