0121b575e809f32af599d84fa264d742d9310481
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2024-06-04 01:26:21 -0400

    solucionado ejercicio

--- ejercicios entrevista.py
+++ ejercicios entrevista.py
@@ -1,10 +1,17 @@
-#definir una funcion max() que tome como argumento 2 numeros
+#definir una funcion maxx() que tome como argumento 2 numeros
 #y devuelva el valor mayor de ellos.
 #(python ya tiene una funcion max incorporada, esto es practica)
 
-def max(num1, num2):
-    if(!num1==int(num1) || !num2==int(num2))
-    if (num1 > num2):
+def maxx(num1, num2):
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+    except :
+        return "Error, ingresar valores numericos"
+    if  num1 > num2 :
         return num1
     else:
-        num2
+        return num2
+print(maxx('a',0))
+
+#
