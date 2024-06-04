f03962652a0478b946d2088efd80473f66b04177
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2024-06-04 01:35:25 -0400

    ejercicio 2, similar, resolvido

--- ejercicios entrevista.py
+++ ejercicios entrevista.py
@@ -13,5 +13,26 @@ def maxx(num1, num2):
     else:
         return num2
 print(maxx('a',0))
+print(maxx(2,0))
+print("----")
+#realizar la misma funcion anterior pero con 3 valores
 
-#
+def max3(num1, num2,num3):
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        num3 = int(num3)
+    except :
+        return "Error, ingresar valores numericos"
+    print(num1,num2,num3)
+    if  num1 > num2 and num1>num3 :
+        return num1
+    elif num2>num3:
+        return num2
+    else:
+        return num3
+print(max3(7,2,3))
+print(max3(1,0,4))
+print(max3(1,9,4))
+
+print("----")
